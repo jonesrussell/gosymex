540afe2f113b842bc4c93063d4aab7df08ec7673
Russell Jones <jonesrussell42@gmail.com>
2023-11-16 17:49:15 -0500

    refactor inspectFile for SRP

--- cmd/analyze.go
+++ cmd/analyze.go
@@ -24,6 +24,53 @@ func parseFile(filePath string) (*ast.File, error) {
 	return parser.ParseFile(fset, filePath, nil, parser.ParseComments)
 }
 
+func handleImportSpec(x *ast.ImportSpec, details *FileDetails) {
+	importPath := strings.Trim(x.Path.Value, "\"")
+	details.Imports = append(details.Imports, importPath)
+}
+
+func handleTypeSpec(x *ast.TypeSpec, details *FileDetails) {
+	switch t := x.Type.(type) {
+	case *ast.StructType:
+		for _, f := range t.Fields.List {
+			field := fmt.Sprintf("%s %s", f.Names[0].Name, types.ExprString(f.Type))
+			details.Structs[x.Name.Name] = append(details.Structs[x.Name.Name], field)
+		}
+	}
+}
+
+func handleFuncDecl(x *ast.FuncDecl, details *FileDetails) {
+	funcSig := fmt.Sprintf("%s(", x.Name.Name)
+	if x.Type.Params != nil {
+		for i, p := range x.Type.Params.List {
+			if i > 0 {
+				funcSig += ", "
+			}
+			for j := range p.Names {
+				if j > 0 {
+					funcSig += ", "
+				}
+				funcSig += fmt.Sprintf("%s %s", p.Names[j], types.ExprString(p.Type))
+			}
+		}
+	}
+	funcSig += ")"
+	if x.Type.Results != nil {
+		funcSig += " returns ("
+		for i, r := range x.Type.Results.List {
+			if i > 0 {
+				funcSig += ", "
+			}
+			if len(r.Names) > 0 {
+				funcSig += fmt.Sprintf("%s ", r.Names[0])
+			}
+			funcSig += types.ExprString(r.Type)
+		}
+		funcSig += ")"
+	}
+	details.Funcs = append(details.Funcs, funcSig)
+}
+
 func inspectFile(filePath string, node *ast.File) *FileDetails {
 	details := &FileDetails{
 		FilePath: filePath,
@@ -33,47 +80,11 @@ func inspectFile(filePath string, node *ast.File) *FileDetails {
 	ast.Inspect(node, func(n ast.Node) bool {
 		switch x := n.(type) {
 		case *ast.ImportSpec:
-			// Remove quotes from import paths
-			importPath := strings.Trim(x.Path.Value, "\"")
-			details.Imports = append(details.Imports, importPath)
+			handleImportSpec(x, details)
 		case *ast.TypeSpec:
-			switch t := x.Type.(type) {
-			case *ast.StructType:
-				for _, f := range t.Fields.List {
-					field := fmt.Sprintf("%s %s", f.Names[0].Name, types.ExprString(f.Type))
-					details.Structs[x.Name.Name] = append(details.Structs[x.Name.Name], field)
-				}
-			}
+			handleTypeSpec(x, details)
 		case *ast.FuncDecl:
-			funcSig := fmt.Sprintf("%s(", x.Name.Name)
-			if x.Type.Params != nil {
-				for i, p := range x.Type.Params.List {
-					if i > 0 {
-						funcSig += ", "
-					}
-					for j := range p.Names {
-						if j > 0 {
-							funcSig += ", "
-						}
-						funcSig += fmt.Sprintf("%s %s", p.Names[j], types.ExprString(p.Type))
-					}
-				}
-			}
-			funcSig += ")"
-			if x.Type.Results != nil {
-				funcSig += " returns ("
-				for i, r := range x.Type.Results.List {
-					if i > 0 {
-						funcSig += ", "
-					}
-					if len(r.Names) > 0 {
-						funcSig += fmt.Sprintf("%s ", r.Names[0])
-					}
-					funcSig += types.ExprString(r.Type)
-				}
-				funcSig += ")"
-			}
-			details.Funcs = append(details.Funcs, funcSig)
+			handleFuncDecl(x, details)
 		}
 		return true
 	})
